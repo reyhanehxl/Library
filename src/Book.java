9b5f7258b69fc60f9ce71123c14a77dafb75f7cd
reyhanehxl <reyhaneh.amanollahii@gmail.com>
2025-03-22 12:33:40 +0330

    Set Book class

---
+++ src/Book.java
@@ -0,0 +1,26 @@
+public class Book {
+    private String title;
+    private int pageCount;
+
+    public Book(String title, int pageCount) {
+        if(pageCount <= 0){
+            throw new IllegalArgumentException("Page count must be positive");
+        }
+        if(title == null || title.trim().isEmpty()){
+            throw new IllegalArgumentException("Title cannot be empty");
+        }
+            this.pageCount = pageCount;
+            this.title = title;
+    }
+    public String getTitle(){
+        return title;
+    }
+
+    public int getPageCount() {
+        return pageCount;
+    }
+    @Override
+    public String toString(){
+        return this.title + " (" + this.pageCount + " pages)";
+    }
+}
